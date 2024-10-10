d5e192b9c85eae6a359018c8f122572b59b02081
LeoAda <69049724+LeoAda@users.noreply.github.com>
2024-10-10 22:48:59 +0200

    save

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,35 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import {
+  ApplicationConfig,
+  Injectable,
+  provideZoneChangeDetection,
+} from '@angular/core';
+import {
+  provideRouter,
+  RouterStateSnapshot,
+  TitleStrategy,
+} from '@angular/router';
 
 import { routes } from './app.routes';
-import { provideClientHydration } from '@angular/platform-browser';
-
+import { provideClientHydration, Title } from '@angular/platform-browser';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+@Injectable({ providedIn: 'root' })
+export class PageTitleStrategy extends TitleStrategy {
+  constructor(private readonly title: Title) {
+    super();
+  }
+  override updateTitle(routerState: RouterStateSnapshot) {
+    const title = this.buildTitle(routerState);
+    if (title !== undefined) {
+      this.title.setTitle(`Leo A.D.A | ${title}`);
+    }
+  }
+}
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    { provide: TitleStrategy, useClass: PageTitleStrategy },
+    provideRouter(routes),
+    provideClientHydration(),
+    provideAnimationsAsync(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { CvComponent } from './pages/cv/cv.component';
+import { BlogComponent } from './pages/blog/blog.component';
+import { LogbookComponent } from './pages/logbook/logbook.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/about', pathMatch: 'full' },
+  { path: 'about', component: HomeComponent, title: 'About' },
+  { path: 'cv', component: CvComponent, title: 'CV' },
+  { path: 'blog', component: BlogComponent, title: 'Blog' },
+  { path: 'logbook', component: LogbookComponent, title: 'Logbook' },
+];
