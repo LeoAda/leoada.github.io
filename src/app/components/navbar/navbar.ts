0b91455b1c1fc04e99d42b547b25746b6f2d8aeb
LeoAda <69049724+LeoAda@users.noreply.github.com>
2026-01-18 15:26:33 +0100

    Add navbar and welcoome page

---
+++ src/app/components/navbar/navbar.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
+import {MatToolbarModule} from '@angular/material/toolbar';
+@Component({
+  selector: 'app-navbar',
+  imports: [MatToolbarModule, MatButtonModule],
+  templateUrl: './navbar.html',
+  styleUrl: './navbar.scss',
+})
+export class Navbar {
+
+}
