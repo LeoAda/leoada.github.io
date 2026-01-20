d3700370f322b045296a539c9bb6a3ed973cceab
LeoAda <69049724+LeoAda@users.noreply.github.com>
2026-01-20 23:35:12 +0100

    Rework education and experience css

--- src/app/pages/education/education.ts
+++ src/app/pages/education/education.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
 import {MatDividerModule} from '@angular/material/divider';
+import {MatChipsModule} from '@angular/material/chips';
 @Component({
   selector: 'app-education',
-  imports: [MatDividerModule],
+  imports: [MatDividerModule, MatChipsModule],
   templateUrl: './education.html',
   styleUrl: './education.scss',
 })
