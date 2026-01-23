dc92cd715223146edd5fc1a07730b15b2cbe2700
LeoAda <69049724+LeoAda@users.noreply.github.com>
2026-01-23 19:30:36 +0100

    Update badges, projects and move home to about

--- src/app/pages/projects/projects.data.ts
+++ src/app/pages/projects/projects.data.ts
@@ -6,7 +6,22 @@ export const personnalProjects: SectionItem[] = [{
     date: 'Feb 2025 - Present',
     description: 'Personal homelab setup',
     tags: [],
-    details: `Description of the homelab project.`,
+    details: `
+Using 2 refurbished Lenovo tiny m90q and a Belink ME mini as nas, I made and maintain a homelab for personal use.
+It is used for hosting tools, personal projects, and media server.
+Having bare metal allows me to learn a lot about hardware, networking and allows me to test very different setups.
+At first I used Proxmox to host Home Assistant, and it was how I got started with homelabs.
+The infrastructure evolved to a kubernetes kluster using k3s.
+I switch over Talos on proxmox for now while I learn more about NixOS.
+I use :
+- traefik as ingress controller
+- longhorn for storage
+- metallb for load balancing
+- fluxcd for gitops in addition to forgejo.
+- prometheus and grafana for monitoring
+
+## Technologies used :
+![Kubernetes](assets/badges/kubernetes.svg) ![Traefik](assets/badges/traefik.svg) ![NixOS](assets/badges/nixos.svg) ![Docker](assets/badges/docker.svg) ![Home Assistant](assets/badges/home_assistant.svg) ![Ubuntu](assets/badges/ubuntu.svg) ![Proxmox](assets/badges/proxmox.svg) ![forgejo](assets/badges/forgejo.svg) ![Grafana](assets/badges/grafana.svg) ![Prometheus](assets/badges/prometheus.svg)`,
 }];
 export const workProjects: SectionItem[] = [];
 export const schoolProjects: SectionItem[] = [];
